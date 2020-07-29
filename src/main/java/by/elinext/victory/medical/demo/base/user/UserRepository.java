93027c14a1c1bad14051ed4d877497488282b4bc
Viktoria Kaltovich <38467721+Victaria@users.noreply.github.com>
2020-07-29 18:25:13 +0300

    after testing

--- src/main/java/by/elinext/victory/medical/demo/base/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package by.elinext.victory.medical.demo.base.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findByEmail(String email);
-}
